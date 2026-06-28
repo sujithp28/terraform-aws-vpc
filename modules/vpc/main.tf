5ce9cffeeb3405d3ed04a0f21796b68e9e1c7d5e
Sujith <70098023+sujithp28@users.noreply.github.com>
2026-06-28 15:54:26 +0530

    feat: Migrate VPC module from terraform-aws-infrastructure

---
+++ modules/vpc/main.tf
@@ -0,0 +1,207 @@
+################################################################################
+# VPC
+################################################################################
+
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = var.enable_dns_support
+  enable_dns_hostnames = var.enable_dns_hostnames
+
+  tags = merge(var.tags, {
+    Name = var.vpc_name
+  })
+}
+
+################################################################################
+# Internet Gateway
+################################################################################
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}-igw"
+  })
+}
+
+################################################################################
+# Public Subnets
+################################################################################
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(var.tags, {
+    Name                     = "${var.vpc_name}-public-${var.azs[count.index]}"
+    "kubernetes.io/role/elb" = "1"
+  })
+}
+
+################################################################################
+# Private Subnets
+################################################################################
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = merge(var.tags, {
+    Name                              = "${var.vpc_name}-private-${var.azs[count.index]}"
+    "kubernetes.io/role/internal-elb" = "1"
+  })
+}
+
+################################################################################
+# Elastic IPs for NAT Gateways
+################################################################################
+
+resource "aws_eip" "nat" {
+  count  = var.enable_nat_gateway ? (var.single_nat_gateway ? 1 : length(var.azs)) : 0
+  domain = "vpc"
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}-eip-${count.index + 1}"
+  })
+
+  depends_on = [aws_internet_gateway.this]
+}
+
+################################################################################
+# NAT Gateways
+################################################################################
+
+resource "aws_nat_gateway" "this" {
+  count = var.enable_nat_gateway ? (var.single_nat_gateway ? 1 : length(var.azs)) : 0
+
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}-nat-${var.azs[count.index]}"
+  })
+
+  depends_on = [aws_internet_gateway.this]
+}
+
+################################################################################
+# Public Route Table
+################################################################################
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}-public-rt"
+  })
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+################################################################################
+# Private Route Tables (one per AZ when multi-NAT)
+################################################################################
+
+resource "aws_route_table" "private" {
+  count  = length(var.private_subnets)
+  vpc_id = aws_vpc.this.id
+
+  dynamic "route" {
+    for_each = var.enable_nat_gateway ? [1] : []
+    content {
+      cidr_block     = "0.0.0.0/0"
+      nat_gateway_id = var.single_nat_gateway ? aws_nat_gateway.this[0].id : aws_nat_gateway.this[count.index].id
+    }
+  }
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}-private-rt-${var.azs[count.index]}"
+  })
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(aws_subnet.private)
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
+
+################################################################################
+# VPC Flow Logs
+################################################################################
+
+resource "aws_cloudwatch_log_group" "flow_logs" {
+  count             = var.enable_flow_logs ? 1 : 0
+  name              = "/aws/vpc/flow-logs/${var.vpc_name}"
+  retention_in_days = var.flow_logs_retention_days
+
+  tags = var.tags
+}
+
+resource "aws_iam_role" "flow_logs" {
+  count = var.enable_flow_logs ? 1 : 0
+  name  = "${var.vpc_name}-flow-logs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "vpc-flow-logs.amazonaws.com"
+      }
+    }]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy" "flow_logs" {
+  count = var.enable_flow_logs ? 1 : 0
+  name  = "${var.vpc_name}-flow-logs-policy"
+  role  = aws_iam_role.flow_logs[0].id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ]
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_flow_log" "this" {
+  count           = var.enable_flow_logs ? 1 : 0
+  iam_role_arn    = aws_iam_role.flow_logs[0].arn
+  log_destination = aws_cloudwatch_log_group.flow_logs[0].arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.this.id
+
+  tags = merge(var.tags, {
+    Name = "${var.vpc_name}-flow-logs"
+  })
+}
